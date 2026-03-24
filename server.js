8fe449a0c4e5749ef4debc0dd27060a158cc0333
Lucas Sória Mouchet <lucassoria080508@gmail.com>
2026-03-23 22:42:52 -0300

    correção do local

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.listen(PORT, () => {
+    console.log(`Server rodando em: ${PORT}`);
+});
+
+app.get('/camisetas', (req, res) => {
+    const camisetas = {
+            id: 1,
+            nome: 'Camiseta Preta',
+            preco: 49.99
+    }
+    res.render("camisetas",{
+        camisetas: camisetas
+    })
+})
