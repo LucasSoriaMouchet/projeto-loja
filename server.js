4abe7fa892a74bd9efe1f4711cced98f07995b07
Lucas Sória Mouchet <lucassoria080508@gmail.com>
2026-03-25 18:40:54 -0300

    -
    update na camisetas.ejs

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.listen(PORT, () => {
 });
 
 app.get('/camisetas', (req, res) => {
-    const camisetas = {
-            id: 1,
-            nome: 'Camiseta Preta',
-            preco: 49.99
-    }
+    const camisetas = [
+        { marca: "BEBIDAS", preco: 49.90, imagem: "camiseta1.jpg" },
+        { marca: "FUMA", preco: 59.90, imagem: "camiseta2.jpg" },
+        { marca: "LACTOSE", preco: 69.90, imagem: "camiseta3.webp" }
+    ]
     res.render("camisetas",{
         camisetas: camisetas
     })
